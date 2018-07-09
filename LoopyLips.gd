28067fd62247a65d77bdbfd6a16045b3753e3310
Nathan <dynamonath@nathanjwtx.me>
2018-07-09 17:37:08 -0500

    Error correction and JSON template
    Replaced dictionary with JSON file. Fixed an error.

--- LoopyLips.gd
+++ LoopyLips.gd
@@ -1,26 +1,29 @@
 extends Node2D
 
 var player_words = []
-var template = [{
-	"inputs": ["a name", "a thing", "a feeling", "another feeling", "some things"],
-	"story": "Once upon a time a %s ate a %s and felt very %s. It was a %s day for all good %s"
-	},
-	{
-	"inputs": ["a name", "a place", "a thing", "another thing", "yet another thing"],
-	"story": "Last week %s went to the %s for a %s. Whilst there a %s fell out of the %s!"
-	}
-	]
 var current_story
 
 func _ready():
-	randomize()
 	reset_textbox()
-	current_story = template[randi() % template.size()]
+	set_random_story()
 	$Background/StoryText.bbcode_enabled = true
 	$Background/StoryText.bbcode_text = ("Welcome to Loopy Lips. \nPlease follow the prompts for a word or phrase."
 		+ "\nCan I have " + current_story.inputs[player_words.size()] + " , please?")
 	
-
+		
+func set_random_story():
+	var stories = get_from_JSON("loopy_lips.JSON")
+	randomize()
+	current_story = stories[randi() % stories.size()]
+	
+func get_from_JSON(filename):
+	var file = File.new()
+	file.open(filename, File.READ)
+	var text = file.get_as_text()
+	var data = parse_json(text)
+	return data
+	file.close()
+	
 func _on_TextBox_text_entered(new_text):
 	player_words.append(new_text)
 	check_number_of_words()
