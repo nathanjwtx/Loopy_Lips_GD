6187a5f09628d17075eb02761594ed6900e83a7f
Nathan <dynamonath@nathanjwtx.me>
2018-07-09 16:15:12 -0500

    Added story template
    Using a dictionary

--- LoopyLips.gd
+++ LoopyLips.gd
@@ -1,19 +1,24 @@
 extends Node2D
 
-var story
 var player_words = []
-var inputs = ["a name", "a thing", "a feeling", "another feeling", "some things"]
-var current_story = {
-	"prompt": ["a name", "a thing", "a feeling", "another feeling", "some things"],
-	"story": ["Once upon a time a %s ate a %s and felt very %s. It was a %s day for all good %s"]
+var template = [{
+	"inputs": ["a name", "a thing", "a feeling", "another feeling", "some things"],
+	"story": "Once upon a time a %s ate a %s and felt very %s. It was a %s day for all good %s"
+	},
+	{
+	"inputs": ["a name", "a place", "a thing", "another thing", "yet another thing"],
+	"story": "Last week %s went to the %s for a %s. Whilst there a %s fell out of the %s!"
 	}
+	]
+var current_story
 
 func _ready():
+	randomize()
 	reset_textbox()
-	story = "Once upon a time a %s ate a %s and felt very %s. It was a %s day for all good %s"
+	current_story = template[randi() % template.size()]
 	$Background/StoryText.bbcode_enabled = true
 	$Background/StoryText.bbcode_text = ("Welcome to Loopy Lips. \nPlease follow the prompts for a word or phrase."
-		+ "\nCan I have " + inputs[player_words.size()] + " , please?")
+		+ "\nCan I have " + current_story.inputs[player_words.size()] + " , please?")
 	
 
 func _on_TextBox_text_entered(new_text):
@@ -32,10 +37,10 @@ func reset_textbox():
 	$Background/TextBox.text = ""
 	
 func prompt_player():
-	$Background/StoryText.bbcode_text = ("Can I have " + inputs[player_words.size()] + " , please?")
+	$Background/StoryText.bbcode_text = ("Can I have " + current_story.inputs[player_words.size()] + " , please?")
 	
 func is_story_done():
-	return player_words.size() == inputs.size()
+	return player_words.size() == current_story.inputs.size()
 	
 func check_number_of_words():
 	if is_story_done():
@@ -47,6 +52,6 @@ func game_over():
 	$Background/TextBox.queue_free()
 	
 func tell_story():
-	$Background/StoryText.bbcode_text = story % player_words
+	$Background/StoryText.bbcode_text = current_story.story % player_words
 	$Background/EnterButton/ButtonLabel.text = "Doogan?"
 	game_over()
